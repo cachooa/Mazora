cfb828b7462ed7d65559fdfe3f7c8d7fe8ec4d1d
cachooa <cachooa@gmail.com>
2024-08-05 00:47:49 +0900

    안드로이드 빌드하던거 올림

--- Assets/Script/DeathPreset.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ActionPreset", menuName = "ScriptableObjects/ActionPreset", order = 1)]
-public class DeathPreset : ScriptableObject
-{
-    public GameObject deathEffectPrefab; // 폭파 이펙트 프리팹
-    public float destroyTime = 2f; // 오브젝트가 삭제될 시간 (초)
-}
--- Assets/Script/IVehicleControl.cs
+++
@@ -1,5 +0,0 @@
-
-public interface IVehicleControl
-{
-    void Control();
-}
